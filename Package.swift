444238c9ac35fd4813525869e29d89ed25a02eae
Kamaal Farah <kamaal.f1@gmail.com>
2020-09-14 08:20:16 +0200

    Upgrading KamaalNetworker

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/kamaal111/KamaalNetworker", from: "1.0.1")
+         .package(url: "https://github.com/kamaal111/KamaalNetworker", from: "2.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
