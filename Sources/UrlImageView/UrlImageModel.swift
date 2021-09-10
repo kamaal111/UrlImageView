a46ab6277523eb99fde2fa844c065112fbdf378b
Kamaal <kamaal@tranzer.com>
2021-09-10 08:59:32 +0200

    Using XiphiasNet 6 implementation

--- Sources/UrlImageView/UrlImageModel.swift
+++ Sources/UrlImageView/UrlImageModel.swift
@@ -14,17 +14,11 @@ final public class UrlImageModel: ObservableObject {
     @Published public var image: UIImage?
 
     private var imageUrl: URL?
-
     private var imageCache = ImageCache.getImageCache()
-
     private var kowalskiAnalysis: Bool
-    private let networker: XiphiasNetable?
 
-    internal init(imageUrl: URL?,
-         networker: XiphiasNetable = XiphiasNet(),
-         kowalskiAnalysis: Bool = false) {
+    internal init(imageUrl: URL?, kowalskiAnalysis: Bool = false) {
         self.kowalskiAnalysis = kowalskiAnalysis
-        self.networker = networker
         self.imageUrl = imageUrl
         self.analyse("\(imageUrl?.absoluteString ?? "") loaded from NSCache")
         let loaded = loadImageFromCache()
@@ -35,7 +29,6 @@ final public class UrlImageModel: ObservableObject {
 
     public init(imageUrl: URL?) {
         self.kowalskiAnalysis = false
-        self.networker = XiphiasNet()
         self.imageUrl = imageUrl
         self.analyse("\(imageUrl?.absoluteString ?? "") loaded from NSCache")
         let loaded = loadImageFromCache()
@@ -58,21 +51,22 @@ private extension UrlImageModel {
 
     func loadImage() {
         guard let imageUrl = imageUrl else { return }
-        DispatchQueue.global().async {
-            self.networker?.loadImage(from: imageUrl) { [weak self] result in
-                switch result {
-                case .failure(let failure):
-                    self?.analyse("*** Failed to load image of \(imageUrl.absoluteString) -> \(failure)")
-                case .success(let imageData):
-                    self?.saveAndSetCachedImage(imageData: imageData, urlString: imageUrl.absoluteString)
-                }
+        DispatchQueue.global().async { [weak self] in
+            guard let self = self else { return }
+            let imageDataResult = XiphiasNet.loadImage(from: imageUrl)
+            switch imageDataResult {
+            case .failure(let failure):
+                self.analyse("*** Failed to load image of \(imageUrl.absoluteString) -> \(failure)")
+            case .success(let success):
+                self.saveAndSetCachedImage(imageData: success, urlString: imageUrl.absoluteString)
             }
         }
     }
 
     func saveAndSetCachedImage(imageData: Data, urlString: String) {
         guard let image = UIImage(data: imageData) else { return }
-        DispatchQueue.main.async {
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
             self.imageCache.set(forKey: urlString, image: image)
             self.image = image
         }
